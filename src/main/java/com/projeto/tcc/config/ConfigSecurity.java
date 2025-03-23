d0dec1acf190fd273c24cf6e30c00e3c6f84bb3b
Arthur Sena <arthur.silva.sena25@gmail.com>
2025-03-23 17:10:47 -0300

    criação das chaves pública e privada e configuração da parte de segurança

---
+++ src/main/java/com/projeto/tcc/config/ConfigSecurity.java
@@ -0,0 +1,69 @@
+package com.projeto.tcc.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@EnableWebSecurity
+public class ConfigSecurity {
+
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "registrar").permitAll())
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+
+    }
+
+
+    @Bean
+    public NimbusJwtDecoder decoder(){
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    public NimbusJwtEncoder encoder(){
+        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder cryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+}
